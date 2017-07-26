fa1c7f83dd91b356977d90200caf4109c95b0009
djtrujillo <danny.j.trujillo@gmail.com>
2017-07-26 09:34:00 -0600

    Iteration 2 completed

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,31 @@
+require 'pry'
 class Activity
+  attr_reader :name,
+              :participants
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participant(name, cost)
+    @participants[name] = cost
+  end
+
+  def total_cost
+    @participants.values.inject(0) do |sum, cost|
+      sum += cost
+    end
+  end
+
+  def split
+    total_cost/@participants.keys.count
+  end
+
+  def owed
+    owed = {}
+    @participants.each do |key, value|
+      owed[key] = (30 - value)
+    end
+    owed
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,45 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def setup
+    @activity = Activity.new("Brunch")
+  end
+
+  def test_activity_initiates_with_name
+    assert_instance_of Activity, @activity
+    assert_equal "Brunch", @activity.name
+  end
+
+  def test_activity_initiates_with_no_participants
+    assert_instance_of Hash, @activity.participants
+  end
+
+  def test_activity_can_add_participant
+    @activity.add_participant("Jim", 20)
+
+    assert_equal 20, @activity.participants["Jim"]
+  end
+
+  def test_activity_adds_total_cost
+    @activity.add_participant("Jim", 20)
+    @activity.add_participant("Joe", 40)
+
+    assert_equal 60, @activity.total_cost
+  end
+
+  def test_activity_splits_the_cost_between_participants
+    @activity.add_participant("Jim", 20)
+    @activity.add_participant("Joe", 40)
+
+    assert_equal 30, @activity.split
+  end
+
+  def test_activity_calculates_how_much_each_participant_owes
+    @activity.add_participant("Jim", 20)
+    @activity.add_participant("Joe", 40)
+
+    assert_equal 10, @activity.owed["Jim"]
+    assert_equal (-10), @activity.owed["Joe"]
+  end
+
 end
